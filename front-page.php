6c1cbf606aed9d9a770e999c0d2a7bc635023a6d
tmerge <tmerge@uos.de>
2020-12-15 00:06:16 +0100

    create frontpage template and add favicon

--- front-page.php
+++ front-page.php
@@ -1,21 +1,16 @@
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * @link    https://tilmerge.xyz/
- */
-?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="de">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri();?>/favicon.ico" />
     <title>Kommunal Bündnis Recke | ...weil es um Reck geht!</title>
-    <?php get_header();?>
+    <?php get_header();?> 
 </head>
 <body>
+    <div class="wrapper">
+        <h1>Willkommen auf der Startseite</h1>
+    </div>
     <?php get_footer();?>
 </body>
 </html>
-
