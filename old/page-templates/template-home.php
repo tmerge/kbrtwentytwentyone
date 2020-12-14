edbec29faafc19c2b65e3a50dfa28e111d34d235
tmerge <tmerge@uos.de>
2020-12-14 23:21:43 +0100

    remove old files and push new one

--- old/page-templates/template-home.php
+++ old/page-templates/template-home.php
@@ -12,6 +12,7 @@
 if ( ! function_exists( 'dynamic_sidebar' ) || ! dynamic_sidebar( 'sidebar-home' ) ): ?>
 	<div class="container p24 wp-caption-text">
 		<h5><?php echo esc_html__( 'This is the "Home Sidebar Section", add some widgets to it to change it.', 'shapely' ); ?></h5>
+		<p>Hello from my side</p>
 	</<div>
 <?php endif; ?>
 
